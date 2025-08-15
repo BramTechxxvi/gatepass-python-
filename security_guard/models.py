a224ac75ffe1c73242a32948f41a9681b6cf7cc4
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-15 12:17:47 +0100

    new

--- security_guard/models.py
+++ security_guard/models.py
@@ -4,7 +4,4 @@ from django.db import models
 # Create your models here.
 
 class SecurityGuard(UserAccount):
-    # first_name = models.CharField(blank=False, null=False)
-    # last_name = models.CharField(blank=False, null=False)
-    # email = models.EmailField(blank=False, null=False, unique=True)
     is_security_guard = models.BooleanField(default=False)
