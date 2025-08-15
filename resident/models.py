c2af69ce0429aa9f20f3b5d3fb665b039065ad81
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-15 11:38:28 +0100

    new

--- resident/models.py
+++ resident/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your models here.
 
@@ -10,9 +12,23 @@ class Resident(User):
     phone = models.CharField(unique=True, blank=False, null=False)
     is_resident = models.BooleanField(default=False)
 
+
 class House(models.Model):
     house_number = models.PositiveIntegerField(blank=False, null=False)
     apartment_number = models.PositiveIntegerField(blank=False, null=False)
     street_name = models.CharField(max_length= 255, blank=False, null=False)
     resident = models.ForeignKey(Resident, on_delete=models.PROTECT)
 
+
+class Invite(models.Model):
+    code = models.CharField(max_length=6, default='', unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(default=timezone.now() + timedelta(hours=8))
+    status = models.BooleanField(default=True)
+    house = models.ForeignKey(House, on_delete=models.PROTECT)
+
+
+class Visitor(models.Model):
+    first_name = models.CharField(blank=False, null=False, max_length=255)
+    last_name = models.CharField(blank=False, null=False, max_length=255)
+    phone = models.CharField(blank=False, null=False, max_length=11)
