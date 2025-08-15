2dab6414a6179e722a5ba5b88075f2df714d64c9
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-15 10:52:18 +0100

    new

--- resident/models.py
+++ resident/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Resident(models.Model):
+class User(models.Model):
     first_name = models.CharField(max_length=100, blank=False, null=False)
     last_name = models.CharField(max_length=100, blank=False, null=False)
     email = models.EmailField(unique=True, blank=False, null=False)
     phone_number = models.CharField(max_length=11, blank=False, null=False)
     password = models.CharField(max_length=16, blank=False, null=False)
+
+
+class Resident(User):
+    pass
