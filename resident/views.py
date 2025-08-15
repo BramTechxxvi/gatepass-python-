1505094cb09d9795a962570ebdc520ed670544ea
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-15 11:55:47 +0100

    new

--- resident/views.py
+++ resident/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your views here.
+
+
+def get_expiry_time() :
+    return timezone.now() + timedelta(hours=10)
--- security_guard/models.py
+++ security_guard/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class SecurityGuard(User):
+    first_name = models.CharField(blank=False, null=False)
+    last_name = models.CharField(blank=False, null=False)
+    email = models.EmailField(blank=False, null=False, unique=True)
+    is_security_guard = models.BooleanField(default=False)
