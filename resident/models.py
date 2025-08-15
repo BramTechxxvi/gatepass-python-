681c5606e4e1b80393ce07efbedc9510f3239ec1
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-15 11:20:38 +0100

    new

--- resident/models.py
+++ resident/models.py
@@ -1,14 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    first_name = models.CharField(max_length=100, blank=False, null=False)
-    last_name = models.CharField(max_length=100, blank=False, null=False)
-    email = models.EmailField(unique=True, blank=False, null=False)
-    phone_number = models.CharField(max_length=11, blank=False, null=False)
-    password = models.CharField(max_length=16, blank=False, null=False)
-
-
 class Resident(User):
-    pass
+    first_name = models.CharField(blank=False, null=False)
+    last_name = models.CharField(blank=False, null=False)
+    email = models.EmailField(unique=True, blank=False, null=False)
+    phone = models.CharField(unique=True, blank=False, null=False)
+    is_resident = models.BooleanField(default=False)
+
+class House(models.Model):
+    house_number = models.PositiveIntegerField(blank=False, null=False)
+    apartment_number = models.PositiveIntegerField(blank=False, null=False)
+    street_name = models.CharField(max_length= 255, blank=False, null=False)
+    resident = models.ForeignKey(Resident, on_delete=models.PROTECT)
+
