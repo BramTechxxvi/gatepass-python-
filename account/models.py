0ecd254478e03d2ffdb6546569196349a1351d90
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-15 12:10:42 +0100

    new

--- account/models.py
+++ account/models.py
@@ -4,3 +4,7 @@ from django.db import models
 # Create your models here.
 
 class UserAccount(AbstractUser):
+    first_name = models.CharField(blank=False, null=False)
+    last_name = models.CharField(blank=False, null=False)
+    email = models.EmailField(blank=False, null=False, unique=True)
+    phone = models.CharField(blank=False, null=False, max_length=11, unique=True)
