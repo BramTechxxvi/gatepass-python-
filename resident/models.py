2095b47b0d6ecf4eae707fbdff164d85c081ea91
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-18 10:56:24 +0100

    new

--- resident/models.py
+++ resident/models.py
@@ -31,5 +31,5 @@ class Invite(models.Model):
 class Visitor(models.Model):
     first_name = models.CharField(blank=False, null=False, max_length=255)
     last_name = models.CharField(blank=False, null=False, max_length=255)
-    phone = models.CharField(blank=False, null=False, max_length=11)
+    phone = models.CharField(blank=False, null=False, max_length=11, unique=True)
     invite = models.ForeignKey(Invite, on_delete=models.PROTECT)
