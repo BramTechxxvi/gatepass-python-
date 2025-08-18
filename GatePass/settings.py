503a14b0da30f0a4f20b48856973b68d93f1e85c
BramTechxxvi <bramtechxxvi@gmail.com>
2025-08-18 11:40:08 +0100

    new

--- GatePass/settings.py
+++ GatePass/settings.py
@@ -81,17 +81,9 @@ WSGI_APPLICATION = 'GatePass.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    },
-    'analytics': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
 }
 
 
